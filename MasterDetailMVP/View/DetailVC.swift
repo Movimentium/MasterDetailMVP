8a3b7e19ae0f64df0c508367376ea55d28c40857
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-06-04 03:05:44 +0200

    Done

--- MasterDetailMVP/View/DetailVC.swift
+++ MasterDetailMVP/View/DetailVC.swift
@@ -17,7 +17,7 @@ class DetailVC: UIViewController {
         // Update the user interface for the detail item.
         if let detail = detailItem {
             if let label = detailDescriptionLabel {
-                label.text = detail.description
+                label.text = detail
             }
         }
     }
@@ -28,7 +28,7 @@ class DetailVC: UIViewController {
         configureView()
     }
 
-    var detailItem: NSDate? {
+    var detailItem: String? {
         didSet {
             // Update the view.
             configureView()
